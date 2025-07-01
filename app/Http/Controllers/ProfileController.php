91bf655109c8cde6c39384d4759a69d994498800
moonblade-art <addinkepri0110@gmail.com>
2025-07-02 06:48:20 +0700

    nambahin "use Illuminate\Support\Facades\log;"

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\log;
 use Illuminate\Validation\ValidationException;
 
 class ProfileController extends Controller
